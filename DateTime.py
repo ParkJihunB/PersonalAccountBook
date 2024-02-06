e8699a2761f4c5b73a24ac5f30c2a11980e8a909
jihun <jihunpark.b@gmail.com>
2024-02-06 17:29:46 +0900

    Created projects.

---
+++ DateTime.py
@@ -0,0 +1,11 @@
+
+class DateTime():
+	year:int
+	month:int
+	day:int
+	meridiem:int
+	hour:int
+	minute:int
+	
+	def __init__(self) -> None:
+		pass
---
+++ JsonManager.py
@@ -0,0 +1,11 @@
+import json
+
+class JsonManager():
+	file_name = "data.json"
+	data = None
+	def __init__(self) -> None:
+		pass
+
+	def load_data(self):
+		with open(self.file_name, encoding='UTF8') as f:
+			self.data = json.load(f)
